1e77973b3a42c67cd76dfbd9ba8255fba55bc4bf
Vadim Sventek <sventek1980@yandex.ru>
2022-03-25 16:23:19 +0300

    cookie finish

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,9 @@
-console.log('Привет');
+const img = document.getElementById('cookie');
+const counter = document.getElementById('clicker__counter')
+img.onclick = () => {
+    counter.textContent = Number(counter.textContent) + 10;
+    img.width += 20;
+    img.heigth += 20; 
+    setTimeout(() => {img.width -= 20;
+        img.heigth -= 20;}, 50);
+}
